ac1d6afff506c4158e0f7c5a813199f969a25c77
lypintopimiento <lypintopimiento@gmail.com>
2021-04-21 19:21:47 -0500

    secuencia_funcciones_26_03_2021_29_03_2021

---
+++ main.js
@@ -0,0 +1,37 @@
+
+
+
+// Codigo en funcion
+function Saludar(p1){
+    console.log(`Hola ${p1}`);
+    return null;
+}
+
+Saludar("Ludwing Pinto");
+Saludar("Maria Camila");
+Saludar("Celina Pimiento");
+Saludar("Jaime Pinto");
+
+
+// Codigo ens ecuencia
+let saludar;
+saludar = "Ludwing Pinto";
+console.warn(`Hola ${saludar}`);
+saludar = "Maria Camila";
+console.warn(`Hola ${saludar}`);
+saludar = "Celina Pimiento";
+console.warn(`Hola ${saludar}`);
+saludar = "Jaime Pinto";
+console.warn(`Hola ${saludar}`);
+
+
+
+// debugger;
+// let nombre;
+// let edad;
+// nombre = prompt("Ingrese su nombre");
+// edad = parseInt(prompt("Ingrese su edad"));
+// console.log(nombre);
+// console.log(typeof(nombre));
+// console.log(edad);
+// console.log(typeof(edad));
